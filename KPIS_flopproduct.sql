937992686e634e75e170483e375ea7dceb84a9dd
Luluscript <159567482+Luluscript@users.noreply.github.com>
2024-06-10 15:32:03 +0200

    Update KPIS_flopproduct.sql

--- KPIS_flopproduct.sql
+++ KPIS_flopproduct.sql
@@ -7,3 +7,16 @@ JOIN products p ON od.productCode = p.productCode
 GROUP BY p.productName
 ORDER BY grossMargin ASC
 LIMIT 5);
+
+-- Marge des moins bonnes ventes
+
+CREATE VIEW flop_product_vw AS
+SELECT p.productName, 
+SUM(od.quantityOrdered) AS totalQuantityOrdered,
+SUM(od.priceEach * od.quantityOrdered) AS sales_product,
+SUM((od.priceEach - p.buyPrice) * od.quantityOrdered) AS grossMargin
+FROM orderdetails od
+JOIN products p ON od.productCode = p.productCode
+GROUP BY p.productName
+ORDER BY sales_product ASC
+LIMIT 5;
