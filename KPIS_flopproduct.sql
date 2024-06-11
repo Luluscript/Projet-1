0e8c4fb94d3455cfe8717b6533959353883f8765
Luluscript <159567482+Luluscript@users.noreply.github.com>
2024-06-11 16:49:44 +0200

    Update KPIS_flopproduct.sql

--- KPIS_flopproduct.sql
+++ KPIS_flopproduct.sql
@@ -1,12 +1,3 @@
--- Marge brut des 5 Flop
-CREATE VIEW flop_product_vw AS
-(SELECT p.productName,
-SUM((od.priceEach - p.buyPrice) * od.quantityOrdered) AS grossMargin
-FROM orderdetails od
-JOIN products p ON od.productCode = p.productCode
-GROUP BY p.productName
-ORDER BY grossMargin ASC
-LIMIT 5);
 
 -- Marge des moins bonnes ventes
 
